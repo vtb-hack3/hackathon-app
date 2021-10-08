a73296d71582a720cfcdeda5bb7f945654873150
Maksim Alekseev <mrvalter566@gmail.com>
2021-10-08 20:47:26 +0700

    Initial Commit

---
+++ HackathonApp/HackathonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonAppApp.swift
+//  HackathonApp
+//
+//  Created by Максим Алексеев  on 08.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HackathonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
