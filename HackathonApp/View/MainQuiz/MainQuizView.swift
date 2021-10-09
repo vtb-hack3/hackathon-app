3ea8ea2c8478f9927abaafa8ab48ecb53bc3a9c8
Maksim Alekseev <mrvalter566@gmail.com>
2021-10-09 19:26:02 +0700

    Create main quiz screen

---
+++ HackathonApp/View/MainQuiz/MainQuizView.swift
@@ -0,0 +1,63 @@
+//
+//  MainQuizView.swift
+//  HackathonApp
+//
+//  Created by Максим Алексеев  on 09.10.2021.
+//
+
+import SwiftUI
+
+struct MainQuizView: View {
+    var body: some View {
+        ZStack {
+            Color("blue_7")
+            VStack {
+                HeaderSectionView()
+                    .padding()
+                Image("EnviroImage")
+                    .resizable()
+                    .frame(height: 400)
+                    .scaledToFit()
+                
+                ZStack {
+                    HStack {
+                        Text("Начинающий")
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            
+                        Image("MedalImage")
+                            .resizable()
+                            .frame(width: 48, height: 39)
+                    }
+                    .frame(width: 339, height: 64)
+                    .background(Color("blue_8"))
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color("orange_border"), lineWidth: 1)
+                    )
+                }
+                .padding(.bottom, 72)
+                
+                ZStack {
+                    Text("Начать викторину")
+                        .foregroundColor(.white)
+                        .fontWeight(.medium)
+                        .font(.system(size: 19))
+                }
+                .frame(width: 343, height: 64)
+                .background(Color("blue_5"))
+                .cornerRadius(16)
+                .shadow(color: Color("shadow_2"), radius: 18, x: 0, y: 0)
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct MainQuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainQuizView()
+    }
+}
