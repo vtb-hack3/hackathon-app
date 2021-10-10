965bce9a40d1daea1706339c6eb501c03eaad396
Vladimir Korolev <korolev.vladimir@skyeng.ru>
2021-10-10 06:50:23 +0300

    minor

--- HackathonApp/View/QuizSearch/QuizSearchView.swift
+++ HackathonApp/View/QuizSearch/QuizSearchView.swift
@@ -11,8 +11,12 @@ struct QuizSearchView: View {
 
     @EnvironmentObject var viewModel: QuizViewModel
     @EnvironmentObject var userViewModel: UserViewModel
+    @State var openNextScreen: Bool = false
 
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var seconds = 10
 
     var body: some View {
         ZStack {
@@ -29,7 +33,7 @@ struct QuizSearchView: View {
                     .offset(x: 0, y: -70)
                     .padding(.bottom, -100)
                     .environmentObject(userViewModel)
-                QuizSearchTimer()
+                QuizSearchTimer(seconds: $seconds)
                 Text("Поиск игрока")
                     .foregroundColor(Color("blue_9"))
                     .font(.system(size: 20))
@@ -51,6 +55,23 @@ struct QuizSearchView: View {
         }
         .edgesIgnoringSafeArea(.top)
         .navigationBarHidden(true)
+        .onReceive(timer) { input in
+            if seconds > 0 {
+                seconds -= 1
+            } else  {
+                timer.upstream.connect().cancel()
+                
+            }
+        }
+        .navigate(
+            to: QuizAboutToStartView(
+                opponent: viewModel.getOpponent(),
+                myPictureId: userViewModel.pictureId,
+                myName: userViewModel.name
+            )
+                .environmentObject(userViewModel),
+            when: $openNextScreen
+        )
     }
 
     private func cancel() {
